e49be3d0174001d454ae06a4e2f652b2520ed92d
Mališa Vučinić <malishav@gmail.com>
2022-02-28 18:25:33 +0100

    Rename EPHEMERAL_KEY_LEN to PUBLIC* and PRIVATE_KEY_LEN.

--- src/main.rs
+++ src/main.rs
@@ -1,7 +1,8 @@
 const MESSAGE_1_LEN: usize = 37;
 const MESSAGE_2_LEN: usize = 45;
 
-const EPHEMERAL_KEY_LEN: usize = 32;
+const PRIVATE_KEY_LEN: usize = 32;
+const PUBLIC_KEY_LEN: usize = PRIVATE_KEY_LEN;
 
 // ciphertext is message_len -1 for c_r, -2 for cbor magic numbers
 const CIPHERTEXT_2_LEN: usize = MESSAGE_2_LEN - EPHEMERAL_KEY_LEN - 1 - 2;
@@ -9,25 +10,24 @@ const CIPHERTEXT_2_LEN: usize = MESSAGE_2_LEN - EPHEMERAL_KEY_LEN - 1 - 2;
 pub fn encode_message_1 <'a> ( buf: &'a mut [u8; MESSAGE_1_LEN],
 								method: u8,
 								suites : u8,
-								g_x : [u8; EPHEMERAL_KEY_LEN],
-								c_i : u8 )
-								-> usize {
-	assert!(MESSAGE_1_LEN > 1 + 1 + EPHEMERAL_KEY_LEN + 1); // length check
+								g_x : [u8; PUBLIC_KEY_LEN],
+								c_i : u8,
+								buf: &'a mut [u8; MESSAGE_1_LEN]) {
+	assert!(MESSAGE_1_LEN > 1 + 1 + PUBLIC_KEY_LEN + 1); // length check
 	assert!(method < 24 && suites < 24 && c_i < 24); // CBOR encoding checks
 
 	buf[0] = method; 	// CBOR unsigned int less than 24 is encoded verbatim
 	buf[1] = suites; 	// CBOR unsigned int less than 24 is encoded verbatim
 	buf[2] = 0x58;		// CBOR byte string magic number
-	buf[3] = EPHEMERAL_KEY_LEN as u8; // length of the byte string
-	for i in 0..EPHEMERAL_KEY_LEN { // copy byte string
+	buf[3] = PUBLIC_KEY_LEN as u8; // length of the byte string
+	for i in 0..PUBLIC_KEY_LEN { // copy byte string
 		buf[4+i] = g_x[i];
 	}
-	buf[4+EPHEMERAL_KEY_LEN] = c_i;	// CBOR uint less than 24 is encoded verbatim
-	MESSAGE_1_LEN
+	buf[4+PUBLIC_KEY_LEN] = c_i;	// CBOR uint less than 24 is encoded verbatim
 }
 
 pub fn parse_message_2 <'a> ( rcvd_message_2: &'a [u8; MESSAGE_2_LEN],
-							   g_y_buf: &'a mut [u8; EPHEMERAL_KEY_LEN],
+							   g_y_buf: &'a mut [u8; PUBLIC_KEY_LEN],
 							   ciphertext_2_buf: &'a mut [u8; CIPHERTEXT_2_LEN],
 							   c_r: &'a mut u8 )
 							   -> Result<u8, i8> {
@@ -35,12 +35,12 @@ pub fn parse_message_2 <'a> ( rcvd_message_2: &'a [u8; MESSAGE_2_LEN],
 
 		*c_r = rcvd_message_2[MESSAGE_2_LEN-1];
 
-		for i in 0 .. EPHEMERAL_KEY_LEN {
+		for i in 0 .. PUBLIC_KEY_LEN {
 			g_y_buf[i] = rcvd_message_2[i+2];
 		}
 
 	for i in 0 .. CIPHERTEXT_2_LEN {
-			ciphertext_2_buf[i] = rcvd_message_2[i + 2 + EPHEMERAL_KEY_LEN];
+			ciphertext_2_buf[i] = rcvd_message_2[i + 2 + PUBLIC_KEY_LEN];
 		}
 
 		Ok(0)
@@ -77,17 +77,29 @@ mod tests {
 						[ 0x0f, 0xf0, 0x4c, 0x29, 0x4f, 0x4a, 0xc6, 0x02,
 						  0xcf, 0x78 ];
 
-	const G_X_TV: [u8; EPHEMERAL_KEY_LEN] =
+	const X_TV: [u8; PRIVATE_KEY_LEN] =
+						[ 0xb3, 0x11, 0x19, 0x98, 0xcb, 0x3f, 0x66, 0x86,
+						  0x63, 0xed, 0x42, 0x51, 0xc7, 0x8b, 0xe6, 0xe9,
+						  0x5a, 0x4d, 0xa1, 0x27, 0xe4, 0xf6, 0xfe, 0xe2,
+						  0x75, 0xe8, 0x55, 0xd8, 0xd9, 0xdf, 0xd8, 0xed ];
+
+	const G_X_TV: [u8; PUBLIC_KEY_LEN] =
 						[ 0x3a, 0xa9, 0xeb, 0x32, 0x01, 0xb3, 0x36, 0x7b,
 						  0x8c, 0x8b, 0xe3, 0x8d, 0x91, 0xe5, 0x7a, 0x2b,
 						  0x43, 0x3e, 0x67, 0x88, 0x8c, 0x86, 0xd2, 0xac,
 						  0x00, 0x6a, 0x52, 0x08, 0x42, 0xed, 0x50, 0x37 ];
 
-	const G_Y_TV : [u8; EPHEMERAL_KEY_LEN] =
+	const G_Y_TV : [u8; PUBLIC_KEY_LEN] =
 						[ 0x25, 0x54, 0x91, 0xb0, 0x5a, 0x39, 0x89, 0xff,
 						  0x2d, 0x3f, 0xfe, 0xa6, 0x20, 0x98, 0xaa, 0xb5,
 						  0x7c, 0x16, 0x0f, 0x29, 0x4e, 0xd9, 0x48, 0x01,
 						  0x8b, 0x41, 0x90, 0xf7, 0xd1, 0x61, 0x82, 0x4e ];
+
+	const G_R_TV : [u8; PUBLIC_KEY_LEN] =
+						[ 0xe6, 0x6f, 0x35, 0x59, 0x90, 0x22, 0x3c, 0x3f,
+						  0x6c, 0xaf, 0xf8, 0x62, 0xe4, 0x07, 0xed, 0xd1,
+						  0x17, 0x4d, 0x07, 0x01, 0xa0, 0x9e, 0xcd, 0x6a,
+						  0x15, 0xce, 0xe2, 0xc6, 0xce, 0x21, 0xaa, 0x50 ];
 
 	#[test]
 	fn test_encode_message_1() {
@@ -102,7 +114,7 @@ mod tests {
 
 	#[test]
 	fn test_parse_message_2() {
-		let mut g_y_buf = [0x00 as u8; EPHEMERAL_KEY_LEN];
+		let mut g_y_buf = [0x00 as u8; PUBLIC_KEY_LEN];
 		let mut ciphertext_2_buf = [0x00 as u8; CIPHERTEXT_2_LEN];
 		let mut c_r = 0xff as u8;
 		let _res = parse_message_2(&MESSAGE_2_TV, &mut g_y_buf, &mut ciphertext_2_buf, &mut c_r);
