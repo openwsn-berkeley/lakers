fcbbb5b1a2ddcb6db4efbd8604cf8445d9a723cb
Mališa Vučinić <malishav@gmail.com>
2022-03-30 10:29:24 +0200

    Environment-related changes to debug with JLink.

--- examples/cc2538-edhoc/flash.sh
+++ examples/cc2538-edhoc/flash.sh
@@ -1,11 +1,12 @@
-#! /usr/bin/sh
+#! /bin/sh
 
-cargo build --target thumbv7m-none-eabi --no-default-features --features cc2538 --example cc2538-edhoc
+cargo build --target thumbv7m-none-eabi
 #cargo build --release
 
 # First convert the elf to binary.
 mkdir -p ./tmp
-llvm-objcopy -O binary $1 ./tmp/flash.bin 
+llvm-objcopy -O binary $1 ./tmp/flash.bin
+#arm-none-eabi-objcopy -O binary $1 ./tmp/flash.bin
 cp $1 ./tmp/flash
 
 # Flash the device
