02e5f94c9910bc4a7c652a54c26aa2c17a60cec9
Mališa Vučinić <malishav@gmail.com>
2022-07-19 18:27:11 +0200

    Fix warnings in edhoc.rs.

--- src/edhoc.rs
+++ src/edhoc.rs
@@ -30,7 +30,7 @@ pub fn edhoc_exporter(
     context_len: usize,
     length: usize,
 ) -> BytesMaxBuffer {
-    let State(x, prk_2e, prk_3e2m, prk_4x3m, h_message_1, th_2, th_3, th_4) = state;
+    let State(_x, _prk_2e, _prk_3e2m, prk_4x3m, _h_message_1, _th_2, _th_3, th_4) = state;
 
     let output = edhoc_kdf(
         &prk_4x3m,
@@ -46,18 +46,17 @@ pub fn edhoc_exporter(
 }
 
 // must hold MESSAGE_1_LEN
-pub fn prepare_message_1(
-    mut state: State,
-) -> (State, BytesMaxBuffer, usize) {
-    let State(mut x, prk_2e, prk_3e2m, prk_4x3m, mut h_message_1, th_2, th_3, th_4) = state;
+pub fn prepare_message_1(mut state: State) -> (State, BytesMaxBuffer, usize) {
+    let State(x, prk_2e, prk_3e2m, prk_4x3m, h_message_1, th_2, th_3, th_4) = state;
 
     // TODO generate ephemeral key
-    x = X;
+    let x = X;
 
     let (buffer, message_1_len) =
         encode_message_1(U8(EDHOC_METHOD), &EDHOC_SUPPORTED_SUITES, &G_X, &C_I);
 
-    h_message_1 = BytesHashLen::from_seq(&hash(&ByteSeq::from_slice(&buffer, 0, message_1_len)));
+    let h_message_1 =
+        BytesHashLen::from_seq(&hash(&ByteSeq::from_slice(&buffer, 0, message_1_len)));
 
     state = construct_state(x, prk_2e, prk_3e2m, prk_4x3m, h_message_1, th_2, th_3, th_4);
 
@@ -70,31 +69,17 @@ pub fn process_message_2(
     mut state: State,
     message_2: &BytesMessage2,
 ) -> (State, bool, BytesCid, U8) {
-    let mut g_y = BytesP256ElemLen::new();
-    let mut ciphertext_2 = BytesCiphertext2::new();
-    let mut c_r = BytesCid::new();
-
     let State(x, mut prk_2e, mut prk_3e2m, mut prk_4x3m, h_message_1, mut th_2, mut th_3, th_4) =
         state;
 
     let (g_y, ciphertext_2, c_r) = parse_message_2(message_2);
 
     // compute prk_2e
-    let mut plaintext_2 = BytesPlaintext2::new();
     prk_2e = compute_prk_2e(&x, &g_y);
-    plaintext_2 = decrypt_ciphertext_2(
-        &prk_2e,
-        &g_y,
-        &c_r,
-        &ciphertext_2,
-        &h_message_1,
-    );
+    let plaintext_2 = decrypt_ciphertext_2(&prk_2e, &g_y, &c_r, &ciphertext_2, &h_message_1);
 
     // decode plaintext_2
-    let mut id_cred_r = U8(0 as u8);
-    let mut mac_2 = BytesMac2::new();
-    let mut ead_2 = BytesEad2::new();
-    let (id_cred_r, mac_2, ead_2) = decode_plaintext_2(&plaintext_2);
+    let (id_cred_r, mac_2, _ead_2) = decode_plaintext_2(&plaintext_2);
 
     // verify mac_2
     prk_3e2m = compute_prk_3e2m(&prk_2e, &x, &G_R);
@@ -127,16 +112,9 @@ pub fn prepare_message_3(
 ) -> (State, BytesMessage3) {
     let State(x, prk_2e, prk_3e2m, prk_4x3m, h_message_1, th_2, th_3, mut th_4) = state;
 
-    let mut mac_3 = BytesMac3::new();
     let mut cred_i_buf = BytesMaxBuffer::new();
     cred_i_buf = cred_i_buf.update(0, cred_i);
-    mac_3 = compute_mac_3(
-        &prk_4x3m,
-        &th_3,
-        id_cred_i,
-        &cred_i_buf,
-        cred_i.len(),
-    );
+    let mac_3 = compute_mac_3(&prk_4x3m, &th_3, id_cred_i, &cred_i_buf, cred_i.len());
 
     let message_3 = compute_bstr_ciphertext_3(&prk_3e2m, &th_3, id_cred_i, &mac_3);
 
@@ -169,7 +147,6 @@ pub fn encode_message_1(
     g_x: &BytesP256ElemLen,
     c_i: &BytesCid,
 ) -> (BytesMaxBuffer, usize) {
-    let mut index: usize = 0;
     let mut output = BytesMaxBuffer::new();
 
     output[0] = method; // CBOR unsigned int less than 24 is encoded verbatim
@@ -204,7 +181,6 @@ pub fn compute_th_2(
     c_r: &BytesCid,
 ) -> BytesHashLen {
     let mut message = BytesMaxBuffer::new();
-    let mut len = 0;
     message[0] = U8(CBOR_BYTE_STRING);
     message[1] = U8(SHA256_DIGEST_LEN as u8);
     message = message.update(2, h_message_1);
@@ -220,7 +196,7 @@ pub fn compute_th_2(
         message[SHA256_DIGEST_LEN + 4 + P256_ELEM_LEN] =
             U8(0x20u8 | (-1i8 - c_r_declassified) as u8);
     }
-    len = SHA256_DIGEST_LEN + 5 + P256_ELEM_LEN;
+    let len = SHA256_DIGEST_LEN + 5 + P256_ELEM_LEN;
 
     let th_2 = BytesHashLen::from_seq(&hash(&ByteSeq::from_slice(&message, 0, len)));
 
@@ -311,48 +287,46 @@ pub fn compute_bstr_ciphertext_3(
     id_cred_i: &BytesIdCred,
     mac_3: &BytesMac3,
 ) -> BytesMessage3 {
-    let mut LABEL_K_3 = BytesMaxLabelBuffer::new();
-    LABEL_K_3[0] = U8(0x4bu8); // 'K'
-    LABEL_K_3[1] = U8(0x5fu8); // '_'
-    LABEL_K_3[2] = U8(0x33u8); // '3'
+    let mut label_k_3 = BytesMaxLabelBuffer::new();
+    label_k_3[0] = U8(0x4bu8); // 'K'
+    label_k_3[1] = U8(0x5fu8); // '_'
+    label_k_3[2] = U8(0x33u8); // '3'
 
-    let mut LABEL_IV_3 = BytesMaxLabelBuffer::new();
-    LABEL_IV_3[0] = U8(0x49u8); // 'I'
-    LABEL_IV_3[1] = U8(0x56u8); // 'V'
-    LABEL_IV_3[2] = U8(0x5fu8); // '_'
-    LABEL_IV_3[3] = U8(0x33u8); // '3'
+    let mut label_iv_3 = BytesMaxLabelBuffer::new();
+    label_iv_3[0] = U8(0x49u8); // 'I'
+    label_iv_3[1] = U8(0x56u8); // 'V'
+    label_iv_3[2] = U8(0x5fu8); // '_'
+    label_iv_3[3] = U8(0x33u8); // '3'
 
-    let mut ENCRYPT0 = Bytes8::new();
-    ENCRYPT0[0] = U8(0x45u8); // 'E'
-    ENCRYPT0[1] = U8(0x6eu8); // 'n'
-    ENCRYPT0[2] = U8(0x63u8); // 'c'
-    ENCRYPT0[3] = U8(0x72u8); // 'r'
-    ENCRYPT0[4] = U8(0x79u8); // 'y'
-    ENCRYPT0[5] = U8(0x70u8); // 'p'
-    ENCRYPT0[6] = U8(0x74u8); // 't'
-    ENCRYPT0[7] = U8(0x30u8); // '0'
+    let mut encrypt0 = Bytes8::new();
+    encrypt0[0] = U8(0x45u8); // 'E'
+    encrypt0[1] = U8(0x6eu8); // 'n'
+    encrypt0[2] = U8(0x63u8); // 'c'
+    encrypt0[3] = U8(0x72u8); // 'r'
+    encrypt0[4] = U8(0x79u8); // 'y'
+    encrypt0[5] = U8(0x70u8); // 'p'
+    encrypt0[6] = U8(0x74u8); // 't'
+    encrypt0[7] = U8(0x30u8); // '0'
 
-    let mut k_3 = BytesMaxBuffer::new();
-    let mut iv_3 = BytesMaxBuffer::new();
     let mut plaintext_3 = BytesPlaintext3::new();
     let mut enc_structure = BytesEncStructureLen::new();
 
     // K_3 = EDHOC-KDF( PRK_3e2m, TH_3, "K_3", h'', key_length )
-    k_3 = edhoc_kdf(
+    let k_3 = edhoc_kdf(
         prk_3e2m,
         th_3,
-        &LABEL_K_3,
-        3,
+        &label_k_3,
+        3, // length of "K_3"
         &BytesMaxContextBuffer::new(),
         0,
         AES_CCM_KEY_LEN,
     );
     // IV_3 = EDHOC-KDF( PRK_3e2m, TH_3, "IV_3", h'', iv_length )
-    iv_3 = edhoc_kdf(
+    let iv_3 = edhoc_kdf(
         prk_3e2m,
         th_3,
-        &LABEL_IV_3,
-        4,
+        &label_iv_3,
+        4, // length of "IV_3"
         &BytesMaxContextBuffer::new(),
         0,
         AES_CCM_IV_LEN,
@@ -362,16 +336,14 @@ pub fn compute_bstr_ciphertext_3(
     plaintext_3[1] = U8(CBOR_MAJOR_BYTE_STRING | MAC_LENGTH_3 as u8);
 
     plaintext_3 = plaintext_3.update(2, mac_3);
-    //    plaintext_3[2..(MAC_LENGTH_3 + 2)].copy_from_slice(&mac_3[..(MAC_LENGTH_3 + 2 - 2)]);
     // encode Enc_structure from draft-ietf-cose-rfc8152bis Section 5.3
     enc_structure[0] = U8(CBOR_MAJOR_ARRAY | 3 as u8); // 3 is the fixed number of elements in the array
-    enc_structure[1] = U8(CBOR_MAJOR_TEXT_STRING | ENCRYPT0.len() as u8);
-    //    enc_structure[2..(ENCRYPT0.len() + 2)].copy_from_slice(&ENCRYPT0[..(ENCRYPT0.len() + 2 - 2)]);
-    enc_structure = enc_structure.update(2, &ENCRYPT0);
-    enc_structure[ENCRYPT0.len() + 2] = U8(CBOR_MAJOR_BYTE_STRING | 0x00 as u8); // 0 for zero-length byte string
-    enc_structure[ENCRYPT0.len() + 3] = U8(CBOR_BYTE_STRING); // byte string greater than 24
-    enc_structure[ENCRYPT0.len() + 4] = U8(SHA256_DIGEST_LEN as u8);
-    enc_structure = enc_structure.update(ENCRYPT0.len() + 5, th_3);
+    enc_structure[1] = U8(CBOR_MAJOR_TEXT_STRING | encrypt0.len() as u8);
+    enc_structure = enc_structure.update(2, &encrypt0);
+    enc_structure[encrypt0.len() + 2] = U8(CBOR_MAJOR_BYTE_STRING | 0x00 as u8); // 0 for zero-length byte string
+    enc_structure[encrypt0.len() + 3] = U8(CBOR_BYTE_STRING); // byte string greater than 24
+    enc_structure[encrypt0.len() + 4] = U8(SHA256_DIGEST_LEN as u8);
+    enc_structure = enc_structure.update(encrypt0.len() + 5, th_3);
 
     let mut output = BytesMessage3::new();
     output[0] = U8(CBOR_MAJOR_BYTE_STRING | CIPHERTEXT_3_LEN as u8);
@@ -414,27 +386,23 @@ pub fn compute_mac_3(
     cred_i: &BytesMaxBuffer,
     cred_i_len: usize,
 ) -> BytesMac3 {
-    let mut LABEL_MAC_3 = BytesMaxLabelBuffer::new();
-    LABEL_MAC_3[0] = U8(0x4du8); // 'M'
-    LABEL_MAC_3[1] = U8(0x41u8); // 'A'
-    LABEL_MAC_3[2] = U8(0x43u8); // 'C'
-    LABEL_MAC_3[3] = U8(0x5fu8); // '_'
-    LABEL_MAC_3[4] = U8(0x33u8); // '3'
+    let mut label_mac_3 = BytesMaxLabelBuffer::new();
+    label_mac_3[0] = U8(0x4du8); // 'M'
+    label_mac_3[1] = U8(0x41u8); // 'A'
+    label_mac_3[2] = U8(0x43u8); // 'C'
+    label_mac_3[3] = U8(0x5fu8); // '_'
+    label_mac_3[4] = U8(0x33u8); // '3'
 
     // MAC_3 = EDHOC-KDF( PRK_4x3m, TH_3, "MAC_3", << ID_CRED_I, CRED_I, ? EAD_3 >>, mac_length_3 )
 
-    let mut context = BytesMaxContextBuffer::new();
-    let mut context_len: usize = 0;
-
-    let (context, context_len) =
-        encode_kdf_context(id_cred_i, cred_i, cred_i_len);
+    let (context, context_len) = encode_kdf_context(id_cred_i, cred_i, cred_i_len);
 
     // compute mac_3
     let output_buf = edhoc_kdf(
         prk_4x3m,
         th_3,
-        &LABEL_MAC_3,
-        5,
+        &label_mac_3,
+        5, // length of "MAC_3"
         &context,
         context_len,
         MAC_LENGTH_3,
@@ -454,7 +422,6 @@ pub fn compute_and_verify_mac_2(
     rcvd_mac_2: &BytesMac2,
 ) -> bool {
     // compute MAC_2
-    let mut mac_2 = BytesMaxBuffer::new();
     let mut label_mac_2 = BytesMaxLabelBuffer::new();
     label_mac_2[0] = U8(0x4du8); // 'M'
     label_mac_2[1] = U8(0x41u8); // 'A'
@@ -462,14 +429,10 @@ pub fn compute_and_verify_mac_2(
     label_mac_2[3] = U8(0x5fu8); // '_'
     label_mac_2[4] = U8(0x32u8); // '2'
 
-    let mut context = BytesMaxContextBuffer::new();
-    let mut context_len: usize = 0;
-
-    let (context, context_len) =
-        encode_kdf_context(id_cred_r, cred_r, cred_r_len);
+    let (context, context_len) = encode_kdf_context(id_cred_r, cred_r, cred_r_len);
 
     // compute mac_2
-    mac_2 = edhoc_kdf(
+    let mac_2 = edhoc_kdf(
         prk_3e2m,
         th_2,
         &label_mac_2,
@@ -491,15 +454,13 @@ pub fn compute_and_verify_mac_2(
     verified
 }
 
-pub fn decode_plaintext_2(
-    plaintext_2: &BytesPlaintext2,
-) -> (U8, BytesMac2, BytesEad2) {
+pub fn decode_plaintext_2(plaintext_2: &BytesPlaintext2) -> (U8, BytesMac2, BytesEad2) {
     let id_cred_r = plaintext_2[0];
     // skip cbor byte string byte as we know how long the string is
     let mut mac_2 = BytesMac2::new();
     mac_2 = mac_2.update_slice(0, plaintext_2, 2, MAC_LENGTH_2);
     // FIXME we don't support ead_2 parsing for now
-    let mut ead_2 = BytesEad2::new();
+    let ead_2 = BytesEad2::new();
 
     (id_cred_r, mac_2, ead_2)
 }
@@ -511,14 +472,10 @@ pub fn decrypt_ciphertext_2(
     ciphertext_2: &BytesCiphertext2,
     h_message_1: &BytesHashLen,
 ) -> BytesPlaintext2 {
-    let mut th_2 = BytesHashLen::new();
-
     // compute the transcript hash th_2
-    th_2 = compute_th_2(h_message_1, g_y, c_r);
+    let th_2 = compute_th_2(h_message_1, g_y, c_r);
 
     // KEYSTREAM_2 = EDHOC-KDF( PRK_2e, TH_2, "KEYSTREAM_2", h'', plaintext_length )
-    let mut keystream_2 = BytesMaxBuffer::new();
-
     let mut label_keystream_2 = BytesMaxLabelBuffer::new();
     label_keystream_2[0] = U8(0x4bu8); // 'K'
     label_keystream_2[1] = U8(0x45u8); // 'E'
@@ -532,7 +489,7 @@ pub fn decrypt_ciphertext_2(
     label_keystream_2[9] = U8(0x5fu8); // '_'
     label_keystream_2[10] = U8(0x32u8); // '2'
 
-    keystream_2 = edhoc_kdf(
+    let keystream_2 = edhoc_kdf(
         prk_2e,
         &th_2,
         &label_keystream_2,
@@ -557,8 +514,7 @@ pub fn compute_prk_4x3m(
     g_y: &BytesP256ElemLen,
 ) -> BytesP256ElemLen {
     // compute g_rx from static R's public key and private ephemeral key
-    let mut g_iy = BytesP256ElemLen::new();
-    g_iy = p256_ecdh(i, g_y);
+    let g_iy = p256_ecdh(i, g_y);
     let prk_4x3m = BytesP256ElemLen::from_seq(&extract(
         &ByteSeq::from_slice(prk_3e2m, 0, prk_3e2m.len()),
         &ByteSeq::from_slice(&g_iy, 0, g_iy.len()),
@@ -573,8 +529,7 @@ pub fn compute_prk_3e2m(
     g_r: &BytesP256ElemLen,
 ) -> BytesP256ElemLen {
     // compute g_rx from static R's public key and private ephemeral key
-    let mut g_rx = BytesP256ElemLen::new();
-    g_rx = p256_ecdh(x, g_r);
+    let g_rx = p256_ecdh(x, g_r);
     let prk_3e2m = BytesP256ElemLen::from_seq(&extract(
         &ByteSeq::from_slice(prk_2e, 0, prk_2e.len()),
         &ByteSeq::from_slice(&g_rx, 0, g_rx.len()),
@@ -583,13 +538,9 @@ pub fn compute_prk_3e2m(
     prk_3e2m
 }
 
-pub fn compute_prk_2e(
-    x: &BytesP256ElemLen,
-    g_y: &BytesP256ElemLen,
-) -> BytesP256ElemLen {
-    let mut g_xy = BytesP256ElemLen::new();
+pub fn compute_prk_2e(x: &BytesP256ElemLen, g_y: &BytesP256ElemLen) -> BytesP256ElemLen {
     // compute the shared secret
-    g_xy = p256_ecdh(x, g_y);
+    let g_xy = p256_ecdh(x, g_y);
     // compute prk_2e as PRK_2e = HMAC-SHA-256( salt, G_XY )
     let prk_2e = BytesP256ElemLen::from_seq(&extract(
         &ByteSeq::new(0),
@@ -599,10 +550,7 @@ pub fn compute_prk_2e(
     prk_2e
 }
 
-fn p256_ecdh(
-    private_key: &BytesP256ElemLen,
-    public_key: &BytesP256ElemLen,
-) -> BytesP256ElemLen {
+fn p256_ecdh(private_key: &BytesP256ElemLen, public_key: &BytesP256ElemLen) -> BytesP256ElemLen {
     let scalar = P256Scalar::from_byte_seq_be(private_key);
     let point = (
         P256FieldElement::from_byte_seq_be(public_key),
@@ -610,7 +558,7 @@ fn p256_ecdh(
     );
 
     // we only care about the x coordinate
-    let (x, y) = p256_point_mul(scalar, point).unwrap();
+    let (x, _y) = p256_point_mul(scalar, point).unwrap();
 
     let secret = BytesP256ElemLen::from_seq(&x.to_byte_seq_be());
     secret
