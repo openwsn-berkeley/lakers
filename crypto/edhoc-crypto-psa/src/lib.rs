49d57708d843caca1bc5ff82b901a108dcb65630
Mališa Vučinić <malishav@gmail.com>
2022-12-08 15:24:40 +0100

    crypto-psa: Implement HMAC as per RFC2104
    HKDF-Extract and HKDF-Expand are not available calls in the PSA API. The
    underlying HMAC isn't available either. To implement HKDF-Extract and
    HKDF-Expand, this commit introduces an implementation of HMAC as per
    RFC2104.

--- crypto/edhoc-crypto-psa/src/lib.rs
+++ crypto/edhoc-crypto-psa/src/lib.rs
@@ -29,9 +29,58 @@ pub fn hkdf_expand(
 }
 
 pub fn hkdf_extract(salt: &BytesHashLen, ikm: &BytesP256ElemLen) -> BytesHashLen {
-    let output = BytesHashLen::new();
+    use psa_crypto::operations::{key_derivation, key_management};
+    use psa_crypto::types::algorithm::{Hash, KeyDerivation};
+    use psa_crypto::types::key::{Attributes, Lifetime, Policy, Type, UsageFlags};
+    use psa_crypto::types::key_derivation::{Input, InputSecret, Inputs, Operation};
 
-    // TODO
+    let mut usage_flags: UsageFlags = Default::default();
+    usage_flags.set_derive();
+    let mut attributes = Attributes {
+        key_type: Type::Derive,
+        bits: 256,
+        lifetime: Lifetime::Volatile,
+        policy: Policy {
+            usage_flags,
+            permitted_algorithms: KeyDerivation::Hkdf {
+                hash_alg: Hash::Sha256,
+            }
+            .into(),
+        },
+    };
+
+    let mut usage_flags: UsageFlags = Default::default();
+    usage_flags.set_derive();
+    let mut derived_key_attributes = Attributes {
+        key_type: Type::RawData,
+        bits: 256, // 32 bytes fixed length in extract
+        lifetime: Lifetime::Volatile,
+        policy: Policy {
+            usage_flags,
+            permitted_algorithms: KeyDerivation::Hkdf {
+                hash_alg: Hash::Sha256,
+            }
+            .into(),
+        },
+    };
+
+    psa_crypto::init().unwrap();
+    let my_key = key_management::import(attributes, None, &ikm.to_public_array()).unwrap();
+    let salt_array = salt.to_public_array();
+    let mut operation = Operation {
+        inputs: Inputs::Hkdf {
+            hash_alg: Hash::Sha256,
+            salt: Some(Input::Bytes(&salt_array)),
+            secret: InputSecret::Input(Input::Key(my_key)),
+            info: Input::Bytes(&[]),
+        },
+        capacity: None,
+    };
+    let _new_key = key_derivation::output_key(operation, derived_key_attributes, None).unwrap();
+
+    let mut output_buf: [u8; SHA256_DIGEST_LEN] = [0; SHA256_DIGEST_LEN];
+    let output_size = key_management::export(_new_key, &mut output_buf).unwrap();
+    let output = BytesHashLen::from_public_slice(&output_buf[..]);
 
     output
 }
@@ -169,4 +218,50 @@ pub fn p256_ecdh(
     let output = BytesP256ElemLen::from_public_slice(&output_buffer[..]);
 
     output
+}
+
+pub fn hmac_sha256(message: &[u8], key: [u8; SHA256_DIGEST_LEN]) -> BytesHashLen {
+    // implementation of HMAC as per RFC2104
+
+    const IPAD: [u8; 64] = [0x36; 64];
+    const OPAD: [u8; 64] = [0x5C; 64];
+
+    //    (1) append zeros to the end of K to create a B byte string
+    //        (e.g., if K is of length 20 bytes and B=64, then K will be
+    //         appended with 44 zero bytes 0x00)
+    let mut B: [u8; MAX_BUFFER_LEN] = [0; MAX_BUFFER_LEN];
+    B[0..SHA256_DIGEST_LEN].copy_from_slice(&key);
+
+    //    (2) XOR (bitwise exclusive-OR) the B byte string computed in step
+    //        (1) with ipad
+    let mut s2: [u8; MAX_BUFFER_LEN] = [0; MAX_BUFFER_LEN];
+    for i in 0..64 {
+        s2[i] = B[i] ^ IPAD[i];
+    }
+
+    //    (3) append the stream of data 'text' to the B byte string resulting
+    //        from step (2)
+    s2[64..64 + message.len()].copy_from_slice(message);
+
+    //    (4) apply H to the stream generated in step (3)
+    let ih = sha256_digest(&BytesMaxBuffer::from_public_slice(&s2), 64 + message.len());
+
+    //    (5) XOR (bitwise exclusive-OR) the B byte string computed in
+    //        step (1) with opad
+    let mut s5: [u8; MAX_BUFFER_LEN] = [0; MAX_BUFFER_LEN];
+    for i in 0..64 {
+        s5[i] = B[i] ^ OPAD[i];
+    }
+    //    (6) append the H result from step (4) to the B byte string
+    //        resulting from step (5)
+    s5[64..64 + SHA256_DIGEST_LEN].copy_from_slice(&ih.to_public_array());
+
+    //    (7) apply H to the stream generated in step (6) and output
+    //        the result
+    let oh = sha256_digest(
+        &BytesMaxBuffer::from_public_slice(&s5),
+        3 * SHA256_DIGEST_LEN,
+    );
+
+    oh
 }
