bd42741a21038182ce8ba8ce65a0ad53ea260ce0
Mališa Vučinić <malishav@gmail.com>
2023-01-02 16:37:59 +0100

    cryptocell310: Implement CCM encrypt

--- crypto/edhoc-crypto-cryptocell310-sys/src/lib.rs
+++ crypto/edhoc-crypto-cryptocell310-sys/src/lib.rs
@@ -6,6 +6,7 @@
 include!(concat!(env!("OUT_DIR"), "/bindings.rs"));
 
 use edhoc_consts::*;
+use hacspec_lib::*;
 
 fn convert_array(input: &[u32]) -> [u8; SHA256_DIGEST_LEN] {
     assert!(input.len() == SHA256_DIGEST_LEN / 4);
@@ -51,7 +52,33 @@ pub fn aes_ccm_encrypt_tag_8(
     ad: &BytesEncStructureLen,
     plaintext: &BytesPlaintext3,
 ) -> BytesCiphertext3 {
-    BytesCiphertext3::new()
+    let mut output = [0x0u8; CIPHERTEXT_3_LEN];
+    let mut tag: CRYS_AESCCM_Mac_Res_t = Default::default();
+    let mut aesccm_key: CRYS_AESCCM_Key_t = Default::default();
+
+    aesccm_key[0..AES_CCM_KEY_LEN].copy_from_slice(&key.to_public_array());
+
+    let err = unsafe {
+        CC_AESCCM(
+            SaSiAesEncryptMode_t_SASI_AES_ENCRYPT,
+            aesccm_key.as_mut_ptr(),
+            CRYS_AESCCM_KeySize_t_CRYS_AES_Key128BitSize,
+            iv.to_public_array().as_mut_ptr(),
+            iv.len() as u8,
+            ad.to_public_array().as_mut_ptr(),
+            ad.len() as u32,
+            plaintext.to_public_array().as_mut_ptr(),
+            plaintext.len() as u32,
+            output.as_mut_ptr(),
+            AES_CCM_TAG_LEN as u8, // authentication tag length
+            tag.as_mut_ptr(),
+            0 as u32, // CCM
+        )
+    };
+
+    output[CIPHERTEXT_3_LEN - AES_CCM_TAG_LEN..].copy_from_slice(&tag[..AES_CCM_TAG_LEN]);
+
+    BytesCiphertext3::from_public_slice(&output)
 }
 
 pub fn aes_ccm_decrypt_tag_8(
