398c12afd36b72558f9fbfdda394cb7f90e31f4f
Mališa Vučinić <malishav@gmail.com>
2023-07-07 18:32:51 +0200

    hotfix! EAD item None should return None

--- ead/edhoc-ead-none/src/lib.rs
+++ ead/edhoc-ead-none/src/lib.rs
@@ -4,7 +4,7 @@ use edhoc_consts::*;
 
 // initiator side
 pub fn i_prepare_ead_1() -> Option<EADItem> {
-    Some(EADItem::new())
+    None
 }
 
 pub fn i_process_ead_2(ead_2: EADItem) -> Result<(), ()> {
@@ -12,7 +12,7 @@ pub fn i_process_ead_2(ead_2: EADItem) -> Result<(), ()> {
 }
 
 pub fn i_prepare_ead_3() -> Option<EADItem> {
-    Some(EADItem::new())
+    None
 }
 
 // responder side
@@ -21,7 +21,7 @@ pub fn r_process_ead_1(ead_1: EADItem) -> Result<(), ()> {
 }
 
 pub fn r_prepare_ead_2() -> Option<EADItem> {
-    Some(EADItem::new())
+    None
 }
 
 pub fn r_process_ead_3(ead_3: EADItem) -> Result<(), ()> {
