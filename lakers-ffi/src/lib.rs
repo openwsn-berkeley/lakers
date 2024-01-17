2f43b74cf4b073e2ae0dca014c71305f0a36b7e9
Geovane Fedrecheski <geonnave@gmail.com>
2024-01-17 14:55:14 +0100

    feat: add basic cbindgen config

--- lakers-ffi/src/lib.rs
+++ lakers-ffi/src/lib.rs
@@ -13,12 +13,12 @@ use panic_semihosting as _;
 // This function is mainly used to test the C wrapper
 #[no_mangle]
 pub extern "C" fn p256_generate_key_pair_from_c(out_private_key: *mut u8, out_public_key: *mut u8) {
-    // let (private_key, public_key) = edhoc_crypto::p256_generate_key_pair();
+    let (private_key, public_key) = default_crypto().p256_generate_key_pair();
 
-    // unsafe {
-    //     // copy the arrays to the pointers received from C
-    //     // this makes sure that data is not dropped when the function returns
-    //     core::ptr::copy_nonoverlapping(private_key.as_ptr(), out_private_key, P256_ELEM_LEN);
-    //     core::ptr::copy_nonoverlapping(public_key.as_ptr(), out_public_key, P256_ELEM_LEN);
-    // }
+    unsafe {
+        // copy the arrays to the pointers received from C
+        // this makes sure that data is not dropped when the function returns
+        core::ptr::copy_nonoverlapping(private_key.as_ptr(), out_private_key, P256_ELEM_LEN);
+        core::ptr::copy_nonoverlapping(public_key.as_ptr(), out_public_key, P256_ELEM_LEN);
+    }
 }
