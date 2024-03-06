7b6220cc8737e1de9471cf2b5c601835b51f2dcb
chrysn <chrysn@fsfe.org>
2024-03-06 14:25:01 +0100

    shared: Remove repr(C) from EDHOCError

--- shared/src/lib.rs
+++ shared/src/lib.rs
@@ -104,7 +104,6 @@ pub type BytesMac = [u8; MAC_LENGTH];
 pub type BytesEncodedVoucher = [u8; ENCODED_VOUCHER_LEN];
 pub type EADMessageBuffer = EdhocMessageBuffer; // TODO: make it of size MAX_EAD_SIZE_LEN
 
-#[repr(C)]
 #[derive(PartialEq, Debug)]
 #[non_exhaustive]
 pub enum EDHOCError {
