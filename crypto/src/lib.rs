551a2814dea26d8d36db89fda1296a835bcab245
Geovane Fedrecheski <geonnave@gmail.com>
2024-03-13 11:34:39 +0100

    fix(crypto/psa): off-by-1 bugs in hkdf_expand

--- crypto/src/lib.rs
+++ crypto/src/lib.rs
@@ -55,3 +55,58 @@ fn test_helper<T: CryptoTrait>() {}
 fn test_implements_crypto() {
     test_helper::<Crypto>()
 }
+
+#[cfg(test)]
+mod tests {
+    use hexlit::hex;
+    use lakers_shared::*;
+
+    use super::*;
+
+    // Test vectors from RFC 5869 (TV1 = test vector for Test Case 1)
+    pub const PRK_TV1: &[u8] =
+        &hex!("077709362c2e32df0ddc3f0dc47bba6390b6c73bb50f9c3122ec844ad7c2b3e5");
+    pub const INFO_TV1: &[u8] = &hex!("f0f1f2f3f4f5f6f7f8f9");
+    pub const L_TV1: usize = 42;
+    pub const OKM_TV1: &[u8] = &hex!(
+        "3cb25f25faacd57a90434f64d0362f2a2d2d0a90cf1a5a4c5db02d56ecc4c5bf34007208d5b887185865"
+    );
+
+    #[test]
+    fn test_hkdf_expand_basic() {
+        let mut crypto = default_crypto();
+
+        let mut prk = [0; SHA256_DIGEST_LEN];
+        prk[..PRK_TV1.len()].copy_from_slice(PRK_TV1);
+        let mut info = [0; MAX_INFO_LEN];
+        info[..INFO_TV1.len()].copy_from_slice(INFO_TV1);
+
+        let mut okm_tv: BytesMaxBuffer = [0; MAX_BUFFER_LEN];
+        okm_tv[..OKM_TV1.len()].copy_from_slice(OKM_TV1);
+
+        let okm = crypto.hkdf_expand(&prk, &info, INFO_TV1.len(), L_TV1);
+        assert_eq!(okm, okm_tv);
+    }
+
+    pub const PRK_TV2: &[u8] =
+        &hex!("06a6b88c5853361a06104c9ceb35b45cef760014904671014a193f40c15fc244");
+    pub const INFO_TV2: &[u8] = &hex!("b0b1b2b3b4b5b6b7b8b9babbbcbdbebfc0c1c2c3c4c5c6c7c8c9cacbcccdcecfd0d1d2d3d4d5d6d7d8d9dadbdcdddedfe0e1e2e3e4e5e6e7e8e9eaebecedeeeff0f1f2f3f4f5f6f7f8f9fafbfcfdfeff");
+    pub const L_TV2: usize = 82;
+    pub const OKM_TV2: &[u8] = &hex!("b11e398dc80327a1c8e7f78c596a49344f012eda2d4efad8a050cc4c19afa97c59045a99cac7827271cb41c65e590e09da3275600c2f09b8367793a9aca3db71cc30c58179ec3e87c14c01d5c1f3434f1d87");
+
+    #[test]
+    fn test_hkdf_expand_long() {
+        let mut crypto = default_crypto();
+
+        let mut prk = [0; SHA256_DIGEST_LEN];
+        prk[..PRK_TV2.len()].copy_from_slice(PRK_TV2);
+        let mut info = [0; MAX_INFO_LEN];
+        info[..INFO_TV2.len()].copy_from_slice(INFO_TV2);
+
+        let mut okm_tv: BytesMaxBuffer = [0; MAX_BUFFER_LEN];
+        okm_tv[..OKM_TV2.len()].copy_from_slice(OKM_TV2);
+
+        let okm = crypto.hkdf_expand(&prk, &info, INFO_TV2.len(), L_TV2);
+        assert_eq!(okm, okm_tv);
+    }
+}
