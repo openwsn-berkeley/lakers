b080dd800d08da351460689cff61d76f4bdeb07a
Geovane Fedrecheski <geonnave@gmail.com>
2024-03-15 11:34:20 +0100

    chore: remove obsolete cc2538 backend and examples

--- examples/lakers-cc2538/gdb_commands.gdb
+++
@@ -1,3 +0,0 @@
-target remote :2331
-file ./tmp/flash
-monitor reset
--- examples/lakers-cc2538/jlink_server.sh
+++
@@ -1,3 +0,0 @@
-#! /bin/sh
-
-JLinkGDBServerCL -Device cc2538sf53 -Speed 4000 -If JTAG -JTAGConf "-1,-1" -LocalHostOnly 1 -Silent 1 -Reset 1 -nohalt
