e599cfadee7f40a412d1605c0a28c9d38e3a8443
chrysn <chrysn@fsfe.org>
2024-05-16 11:18:44 +0200

    r_parse_message_3: Tolerate unparsable credential-by-value
    Users must already be aware that what is returned could be incomplete
    (i.e. have no key) because it could be a credential by key ID; now it
    could also be a credential-by-value that is not in a format that the
    limited Lakers credentials parser recognizes.

--- lib/src/edhoc.rs
+++ lib/src/edhoc.rs
@@ -173,7 +173,15 @@ pub fn r_parse_message_3(
                     let Ok(buffer) = EdhocMessageBuffer::new_from_slice(cred) else {
                         return Err(EDHOCError::ParsingError);
                     };
-                    CredentialRPK::new(buffer)?
+                    if let Ok(parsed_rpk) = CredentialRPK::new(buffer) {
+                        parsed_rpk
+                    } else {
+                        CredentialRPK {
+                            value: buffer,
+                            public_key: Default::default(),
+                            kid: Default::default(),
+                        }
+                    }
                 }
             };
 
