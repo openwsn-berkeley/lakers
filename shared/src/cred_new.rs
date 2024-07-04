95fb363c5805f706c1427037cc027131931d708a
Geovane Fedrecheski <geonnave@gmail.com>
2024-07-04 14:37:35 +0200

    creds: new id_cred struct improvements

--- shared/src/cred_new.rs
+++ shared/src/cred_new.rs
@@ -24,22 +24,69 @@ pub enum CredentialType {
     // C509,
 }
 
+#[derive(Clone, Copy, Debug, PartialEq)]
+pub enum IdCredType {
+    KID = 4,
+    KCCS = 14,
+}
+
+impl From<u8> for IdCredType {
+    fn from(value: u8) -> Self {
+        match value {
+            4 => IdCredType::KID,
+            14 => IdCredType::KCCS,
+            _ => panic!("Invalid IdCredType"),
+        }
+    }
+}
+
 /// A value of ID_CRED_x: a credential identifier
 ///
 /// Possible values include key IDs, credentials by value and others.
 // TODO: rename to just IdCred
-pub struct StructIdCredNew {
+pub struct IdCredNew {
     /// The value is always stored in the ID_CRED_x form as a serialized one-element dictionary;
     /// while this technically wastes two bytes, it has the convenient property of having the full
     /// value available as a slice.
     pub bytes: BufferIdCred, // variable size, can contain either the contents of a BufferCred or a BufferKid
 }
 
-impl StructIdCredNew {
+impl IdCredNew {
     pub fn new() -> Self {
         Self {
             bytes: BufferIdCred::new(),
         }
+    }
+
+    /// Instantiate an IdCredNew from an encoded value.
+    pub fn from_encoded_plaintext(value: &[u8]) -> Result<Self, EDHOCError> {
+        let bytes = match value {
+            // kid that has been encoded as CBOR integer
+            &[x] if x < 24 => {
+                BufferIdCred::new_from_slice(&[0xa1, KID_LABEL, 0x41, x])
+                    .map_err(|_| EDHOCError::CredentialTooLongError)? // TODO: make this error handling less verbose?
+            }
+            // kid that has been encoded as CBOR byte string
+            &[0x41, ..] => {
+                let mut bytes = BufferIdCred::new_from_slice(&[0xa1, KID_LABEL])
+                    .map_err(|_| EDHOCError::CredentialTooLongError)?;
+                bytes
+                    .extend_from_slice(value)
+                    .map_err(|_| EDHOCError::CredentialTooLongError)?;
+                bytes
+            }
+            // credential by value
+            value => {
+                let mut bytes = BufferIdCred::new_from_slice(&[0xa1, KCSS_LABEL])
+                    .map_err(|_| EDHOCError::CredentialTooLongError)?;
+                bytes
+                    .extend_from_slice(value)
+                    .map_err(|_| EDHOCError::CredentialTooLongError)?;
+                bytes
+            }
+        };
+
+        Ok(Self { bytes })
     }
 
     /// View the full value of the ID_CRED_x: the CBOR encoding of a 1-element CBOR map
@@ -49,12 +96,19 @@ impl StructIdCredNew {
 
     /// View the value as encoded in the ID_CRED_x position of plaintext_2 and plaintext_3,
     /// applying the Compact Encoding of ID_CRED Fields described in RFC9528 Section 3.5.3.2
-    pub fn as_compact_encoding(&self) -> &[u8] {
+    /// Note that this does NOT encode the value as CBOR, it rather just applies the EDHOC Compact Encoding when applicable.
+    pub fn encode_for_plaintext(&self) -> &[u8] {
         match self.bytes.as_slice() {
-            [0xa1, 0x04, 0x41, x] if (x >> 5) < 2 && (x & 0x1f) < 24 => &self.bytes.as_slice()[3..],
-            [0xa1, 0x04, ..] => &self.bytes.as_slice()[2..],
+            [0xa1, KID_LABEL, 0x41, x] if (x >> 5) < 2 && (x & 0x1f) < 24 => {
+                &self.bytes.as_slice()[3..]
+            }
+            [0xa1, KID_LABEL, ..] => &self.bytes.as_slice()[2..],
             _ => self.bytes.as_slice(),
         }
+    }
+
+    pub fn item_type(&self) -> IdCredType {
+        self.bytes.as_slice()[1].into()
     }
 }
 
@@ -171,14 +225,15 @@ impl Credential {
             Err(EDHOCError::ParsingError)
         }
     }
+
     /// Returns a COSE_Header map with a single entry representing a credential by value.
     ///
     /// For example, if the credential is a CCS:
     ///   { /kccs/ 14: bytes }
-    pub fn by_value(&self) -> Result<StructIdCredNew, EDHOCError> {
+    pub fn by_value(&self) -> Result<IdCredNew, EDHOCError> {
         match self.cred_type {
             CredentialType::CCS => {
-                let mut id_cred = StructIdCredNew::new();
+                let mut id_cred = IdCredNew::new();
                 id_cred
                     .bytes
                     .extend_from_slice(&[CBOR_MAJOR_MAP + 1, KCSS_LABEL])
@@ -201,11 +256,11 @@ impl Credential {
     ///   { /kid/ 4: kid }
     ///
     /// TODO: accept a parameter to specify the type of reference, e.g. kid, x5t, etc.
-    pub fn by_kid(&self) -> Result<StructIdCredNew, EDHOCError> {
+    pub fn by_kid(&self) -> Result<IdCredNew, EDHOCError> {
         let Some(kid) = self.kid.as_ref() else {
             return Err(EDHOCError::MissingIdentity);
         };
-        let mut id_cred = StructIdCredNew::new();
+        let mut id_cred = IdCredNew::new();
         id_cred
             .bytes
             .extend_from_slice(&[
@@ -228,6 +283,7 @@ impl Credential {
 mod test {
     use super::*;
     use hexlit::hex;
+    use rstest::rstest;
 
     const CRED_TV: &[u8] = &hex!("a2026b6578616d706c652e65647508a101a501020241322001215820bbc34960526ea4d32e940cad2a234148ddc21791a12afbcbac93622046dd44f02258204519e257236b2a0ce2023f0931f1f386ca7afda64fcde0108c224c51eabf6072");
     const G_A_TV: &[u8] = &hex!("BBC34960526EA4D32E940CAD2A234148DDC21791A12AFBCBAC93622046DD44F0");
@@ -252,8 +308,10 @@ mod test {
             .with_kid(KID_VALUE_TV.try_into().unwrap());
         let id_cred = cred.by_value().unwrap();
         assert_eq!(id_cred.bytes.as_slice(), ID_CRED_BY_VALUE_TV);
+        assert_eq!(id_cred.item_type(), IdCredType::KCCS);
         let id_cred = cred.by_kid().unwrap();
         assert_eq!(id_cred.bytes.as_slice(), ID_CRED_BY_REF_TV);
+        assert_eq!(id_cred.item_type(), IdCredType::KID);
     }
 
     #[test]
@@ -288,5 +346,18 @@ mod test {
         //     CredentialKey::Symmetric(K.try_into().unwrap())
         // );
         // assert_eq!(cred.cred_type, CredentialType::CCS_PSK);
+    }
+
+    #[rstest]
+    #[case(&[0x0D], &[0xa1, 0x04, 0x41, 0x0D])] // two optimizations: omit kid label and encode as CBOR integer
+    #[case(&[0x41, 0x18], &[0xa1, 0x04, 0x41, 0x18])] // one optimization: omit kid label
+    #[case(CRED_TV, ID_CRED_BY_VALUE_TV)] // regular credential by value
+    fn test_id_cred_from_encoded_plaintext(#[case] input: &[u8], #[case] expected: &[u8]) {
+        assert_eq!(
+            IdCredNew::from_encoded_plaintext(input)
+                .unwrap()
+                .as_full_value(),
+            expected
+        );
     }
 }
