c49ccea15278f23d17c5c732f47512d6ded6fc25
Mališa Vučinić <malishav@gmail.com>
2024-07-12 14:16:06 +0200

    fix(example): align the coap client-server native examples

--- examples/coap/src/bin/coapclient.rs
+++ examples/coap/src/bin/coapclient.rs
@@ -30,8 +30,8 @@ fn client_handshake() -> Result<(), EDHOCError> {
     let timeout = Duration::new(5, 0);
     println!("Client request: {}", url);
 
-    let cred_i = CredentialRPK::new(CRED_I.try_into().unwrap()).unwrap();
-    let cred_r = CredentialRPK::new(CRED_R.try_into().unwrap()).unwrap();
+    let cred_i: Credential = Credential::parse_ccs(CRED_I.try_into().unwrap()).unwrap();
+    let cred_r: Credential = Credential::parse_ccs(CRED_R.try_into().unwrap()).unwrap();
 
     let initiator = EdhocInitiator::new(
         lakers_crypto::default_crypto(),
@@ -54,9 +54,10 @@ fn client_handshake() -> Result<(), EDHOCError> {
     println!("message_2 len = {}", response.message.payload.len());
 
     let message_2 = EdhocMessageBuffer::new_from_slice(&response.message.payload[..]).unwrap();
-    let (initiator, c_r, id_cred_r, _ead_2) = initiator.parse_message_2(&message_2)?;
+    let (mut initiator, c_r, id_cred_r, _ead_2) = initiator.parse_message_2(&message_2)?;
     let valid_cred_r = credential_check_or_fetch(Some(cred_r), id_cred_r).unwrap();
-    let initiator = initiator.verify_message_2(&I, cred_i, valid_cred_r)?;
+    initiator.set_identity(I.try_into().unwrap(), cred_i);
+    let initiator = initiator.verify_message_2(valid_cred_r)?;
 
     let mut msg_3 = Vec::from(c_r.as_cbor());
     let (mut initiator, message_3, prk_out) =
