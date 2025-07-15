b2f7ddd8a15b39604d66e8a4ecc4e984fa19d2c2
chrysn <chrysn@fsfe.org>
2025-07-15 12:13:18 +0200

    Address pyo3 deprecations around the IntoPyObject migration

--- shared/src/python_bindings.rs
+++ shared/src/python_bindings.rs
@@ -71,18 +71,19 @@ impl<'a, 'py> pyo3::conversion::FromPyObject<'py> for EadItems {
     }
 }
 
-impl pyo3::conversion::IntoPy<PyObject> for EadItems {
-    fn into_py(self, py: Python<'_>) -> PyObject {
-        let list = pyo3::types::PyList::new(py, core::iter::empty::<PyObject>())
-            .expect("No items are appended that could err");
+impl<'py> pyo3::conversion::IntoPyObject<'py> for EadItems {
+    type Target = pyo3::types::PyList;
+    type Output = Bound<'py, Self::Target>;
+    type Error = PyErr;
+
+    fn into_pyobject(self, py: Python<'py>) -> Result<Self::Output, Self::Error> {
+        let list = pyo3::types::PyList::new(py, core::iter::empty::<PyObject>())?;
         // Can't pass it into new as it doesn't have an ExactSizeItertor -- FIXME: should we
         // implement and use that?
         for item in self.iter() {
-            list.append(item.clone().into_py(py))
-                // ... and we can't return an error here anyway.
-                .expect("Appending to a Python list does not realistically err");
+            list.append(item.clone())?;
         }
-        list.into()
+        Ok(list.into())
     }
 }
 
